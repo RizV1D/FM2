73f9d3d9c276f8f16c0c83fa736b1d985e286e68
RizV1D <139112984+RizV1D@users.noreply.github.com>
2023-07-20 11:03:21 +0700

    Add files via upload

---
+++ FM.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+sleep 3
+clear
+
+figlet FM
+echo "use this tool wisely"
+echo "========================================"
+echo "       {•} Author : Muhammad Rizky"
+echo "       {•}  YT    :  Cream21"
+echo "========================================"
+echo "                  Menu                 "
+echo "[1].Login Facebook"
+echo "[2].Whatshap hack"
+echo "[3].Spam Call"
+echo "[4].DDOS"
+echo "[5].Spam Brutal [By:Vindra ID]"
+echo "[6].Spam Call [By:Vindra ID]"
+echo "[7].install FM2"
+
+read -p "pilih : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo "Cooming soon"
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo "don't use this tool wrong"
+read -p "y/n : " wv
+clear
+sleep 2
+echo "################################"
+echo "#       Whatshap Hack          #"
+echo "#  {•} Author : Muhammad Rizky #"
+echo "#  {•}   YT   : Cream21        #"
+echo "#                              #"
+echo "#                              #"
+echo "################################"
+read -p "number You : " ny
+read -p "number Target : " nt
+sleep 3
+echo "[*]Scanne!!"
+sleep 8
+echo "[*]Loading.."
+sleep 3
+echo "[*]Erorr!!"
+echo "enter the number correctly"
+echo "Erorr"
+exit
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+figlet spam Call
+read -p "number Target : " op
+sleep 5
+echo "[*].Done"
+sleep 5
+echo "[*].Done"
+exit
+fi
+
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+echo "don't use this tool wrong"
+cd LITEDDOS
+read -p "alamat ip : " ak
+echo "Erorr"
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+cd Brutal-Spam
+python run.py
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+cd Prank-SSC
+python run.py
+echo "gua hanya nyebar script bukan mengambil script"
+fi
+
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+pkg install git
+sleep 2
+clear
+figlet FM2
+echo "#########################################################"
+echo "#------------------------[FM2]--------------------------#"
+echo "#-------------------------------------------------------#"
+echo "#   Command:           <y>          <n>                 #"
+echo "#                                                       #"
+echo "#Creator:M.Rizky    ##      ###       ##                #"
+echo "#Team   : Trm        ##     #          ##               #"
+echo "#Version:0.2        ##      ###       ##                #"
+echo "#                   ##  ##     #  ##  ##                #"
+echo "#                   ##  ##  ###   ##  ######            #"
+echo "#               <--[Indonesia Security]-->              #"
+echo "#########################################################"
+echo "                        @@@@@@@@@@"
+echo "                       @@@@@@@@@@@@"
+echo "                     @@@@@@@@@@@@@@@@"
+echo "Do you want your cellphone to be rooted?"
+read -p "y/n : " oqh
+pkg install git
+pkg install python2
+pkg install requests
+sleep 3
+clear
+figlet ROOT Succes
+echo "your phone is already rooted"
+exit
+fi
